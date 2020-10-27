dcd9def4e2490b9c47725228bdd14b1a5d71abd5
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 18:29:30 +0530

    initial commit

---
+++ Stop-AzVM.ps1
@@ -0,0 +1,27 @@
+﻿<#
+    Stops one or more Azure VMs.
+#>
+
+# Names of the Azure VMs to stop
+# TODO: replace with your Azure VM / service names
+$VMsToStop = @(
+    @{
+        Name = "SomeVMName"
+        ServiceName = "SomeVMServiceName"
+    },
+    @{
+        Name = "SomeVMName2"
+        ServiceName = "SomeVMServiceName2"
+    }
+)
+
+# Get creds to access Azure
+$AzureCred = Get-AutomationPSCredential -Name "AzureCreds"
+
+# Connect to Azure
+Add-AzureAccount -Credential $AzureCred | Out-Null
+
+# Stop the VMs, in parallel
+ForEach ($VM in $VMsToStop) {
+    Stop-AzureVM -Name $VM.Name -ServiceName $VM.ServiceName -Force
+}
